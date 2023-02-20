3b351539052f0e079ff3a7cdb784c0e7c486d83c
Samuel <sambeti2002@gmail.com>
2023-02-20 13:10:30 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Determines if a number
+ *
+ * Description: to print out if statement
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z')
+		{
+			puchar(d);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
