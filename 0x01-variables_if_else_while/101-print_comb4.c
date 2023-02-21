545faae868eeb61503afbe87bbdde65bf4039636
Samuel <sambeti2002@gmail.com>
2023-02-21 02:45:47 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Determines if a number
+ *
+ * Description: to print out if statement
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	int b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 8 && b == 9)
+
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
