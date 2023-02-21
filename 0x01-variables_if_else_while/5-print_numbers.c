6de2f3377fff3266e881a23c27d2ba49a44664f4
Samuel <sambeti2002@gmail.com>
2023-02-21 01:49:56 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Determines if a number
+ *
+ * Description: to print out if statement
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
