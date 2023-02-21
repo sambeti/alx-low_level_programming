dfe257fb0e3d1ecea319c55317fa8ecc8484e5eb
Samuel <sambeti2002@gmail.com>
2023-02-21 03:05:34 -0800

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,19 +12,25 @@ int main(void)
 
 	int b;
 
+	int c;
+
 	for (a = 0; a < 9; a++)
 	{
 		for (b = a + 1; b < 10; b++)
 		{
+			for (c = '0'; c < '9'; c++)
+			{
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
+			putchar((c % 10) + '0');
 
-			if (a == 8 && b == 9)
+			if (a == '9' && b == '9' && c == '8')
 
 				continue;
 
 			putchar(',');
 			putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
