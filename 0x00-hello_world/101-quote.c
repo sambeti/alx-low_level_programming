6d9b3f83f8b0e05f37bfbac3ff01c36756b7f01e
Samuel <sambeti2002@gmail.com>
2023-02-21 06:05:37 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(2, message, sizeof(message) - 1);
+  return (1);
 }
